2e40e939ebae0e33b58eca0f38b52e6443216e62
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-03-29 21:35:32 +0000

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        logger.info("Demo app started! Go to http://localhost:8080/dic_student_name");
+    }
+
+}
+
+@RestController
+@RequestMapping("/dic_student_name")
+class DemoController {
+
+    @Value("${fullname:Bass}")
+    private String fullname;
+
+    @GetMapping
+    String getFullname() {
+        return "Work done by " + this.fullname + "!";
+    }
+}
+
+
